d6e6c796f6e2ef2970c6f9f61ae43d71a0a6d1e5
Leonard Lopez <llopez@nube.gob.ve>
2025-03-06 16:26:22 -0400

    [feat] add OutlinedTextFieldH component

---
+++ app/src/main/java/com/leonardlm/habbitsapp/core/presentation/components/atoms/OutlinedTextFieldH.kt
@@ -0,0 +1,93 @@
+package com.leonardlm.habbitsapp.core.presentation.components.atoms
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.text.input.KeyboardType
+import java.lang.Error
+
+@Composable
+fun OutlinedTextFieldH(
+    value: String,
+    onValueChange: (String) -> Unit,
+    isError: Boolean = false,
+    label: @Composable() (() -> Unit)?,
+    placeholder: String,
+    contentDescription: String,
+    errorMessage: String,
+    isEnable: Boolean = true,
+    keyboardType: KeyboardType = KeyboardType.Text,
+    keyboardOptions: KeyboardOptions = KeyboardOptions(),
+    keyboardActions: KeyboardActions = KeyboardActions(),
+    modifier: Modifier = Modifier,
+    passwordVisibility: Boolean = false,
+    passwordVisibilityOnClick: (Boolean) -> Unit = {},
+    startIcon: ImageVector? = null,
+    endIcon: ImageVector? = null,
+    leadingIcon: @Composable() (() -> Unit)? = if (startIcon != null)  {
+        {
+            Icon(
+                imageVector = startIcon,
+                contentDescription = contentDescription
+            )
+        }
+    } else null,
+    trailingIcon: @Composable() (() -> Unit)? = if (endIcon != null || keyboardType.toString() == KeyboardType.Password.toString()) {
+        {
+            if (keyboardType.toString() == KeyboardType.Password.toString()) {
+
+                val msg =
+                    if (passwordVisibility) "Hide" else "Show"
+
+                IconButton(onClick = { passwordVisibilityOnClick(passwordVisibility) }
+                ) {
+                    Icon(
+                        imageVector = endIcon!!,
+                        contentDescription = null
+                    )
+                    Text(
+                        text = msg,
+                        color = Color.Red
+                    )
+                }
+            } else {
+                Icon(
+                    imageVector = endIcon!!,
+                    contentDescription = null,
+                    tint = Color(0xFF000000)
+                )
+            }
+        }
+    } else null,
+) {
+    Column {
+        OutlinedTextField(
+            value = value,
+            onValueChange = onValueChange,
+            modifier = Modifier
+                .fillMaxWidth()
+                .semantics { this.contentDescription = contentDescription },
+            label = label,
+            placeholder = { Text(text = placeholder) },
+            isError = errorMessage.isNotEmpty(),
+            singleLine = true,
+            enabled = isEnable,
+            keyboardOptions = keyboardOptions,
+            keyboardActions = keyboardActions,
+            leadingIcon = leadingIcon,
+            trailingIcon = trailingIcon,
+        )
+    }
+}
+
