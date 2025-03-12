66c9b0d2dc175f798c83c374b513f08136f9815f
Leonard Lopez <llopez@nube.gob.ve>
2025-03-12 12:58:46 -0400

    [refactor] remove IconButton to OutlinedTextFieldH

--- app/src/main/java/com/leonardlm/habbitsapp/core/presentation/components/atoms/OutlinedTextFieldH.kt
+++ app/src/main/java/com/leonardlm/habbitsapp/core/presentation/components/atoms/OutlinedTextFieldH.kt
@@ -7,16 +7,17 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Lock
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
@@ -24,7 +25,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.semantics.contentDescription
 import androidx.compose.ui.semantics.semantics
-import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
@@ -63,15 +63,11 @@ fun OutlinedTextFieldH(
                 val msg =
                     if (passwordVisibility) "Hide" else "Show"
 
-                IconButton(onClick = { passwordVisibilityOnClick(passwordVisibility) }
+                TextButton(onClick = { passwordVisibilityOnClick(passwordVisibility) }
                 ) {
-                    Icon(
-                        imageVector = endIcon!!,
-                        contentDescription = null
-                    )
                     Text(
                         text = msg,
-                        color = Color.Red
+                        color = MaterialTheme.colorScheme.tertiary
                     )
                 }
             } else {
@@ -160,7 +156,8 @@ fun HabitPasswordTextField(
         },
         keyboardOptions = keyboardOptions,
         keyboardActions = keyboardActions,
-        backgroundColor = backgroundColor
+        backgroundColor = backgroundColor,
+        startIcon = Icons.Outlined.Lock
     )
 
 }
