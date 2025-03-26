3cca84356735f31fca4953f367b51fe32f34733f
Leonard Lopez <llopez@nube.gob.ve>
2025-03-26 16:02:21 -0400

    [feat] add HomeDateSelector component

--- app/src/main/java/com/leonardlm/habbitsapp/home/presentation/main/components/HomeDateSelector.kt
+++ app/src/main/java/com/leonardlm/habbitsapp/home/presentation/main/components/HomeDateSelector.kt
@@ -1,7 +1,10 @@
 package com.leonardlm.habbitsapp.home.presentation.main.components
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import java.time.ZonedDateTime
 
 @Composable
@@ -10,7 +13,21 @@ fun HomeDateSelector(
     mainDate: ZonedDateTime,
     onDateClick: (ZonedDateTime) -> Unit,
     modifier: Modifier = Modifier,
-    datesToShow: Int = 5
+    datesToShow: Int = 4
 ) {
-
+    Row(
+        modifier = modifier,
+        horizontalArrangement = Arrangement.spacedBy(6.dp)
+    ) {
+        for (i in datesToShow downTo 0) {
+            val date = mainDate.minusDays(i.toLong())
+            HomeDateItem(
+                date = date,
+                selected = date.toLocalDate() == selectedDate.toLocalDate(),
+                onClick = {
+                    onDateClick(date)
+                }
+            )
+        }
+    }
 }
